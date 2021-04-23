a0072e10cc39d5f49d6c1954951088b80f05d85d
Ritabrata Das <dasritabrata5@gmail.com>
2021-04-23 22:35:42 +0530

    Update views.py

--- Database_app/enroll/views.py
+++ Database_app/enroll/views.py
@@ -21,6 +21,23 @@ def add_show(request):
     river = User.objects.all()
     return render (request, 'enroll/addandshow.html', {'form': ob, 'stu': river})
 
+def show_data(request):
+    if request.method == 'POST':
+        river = ''
+        ob = StudentRegistration(request.POST)
+        if ob.is_valid():
+            nm = ob.cleaned_data['name']
+            em = ob.cleaned_data['email']
+            bd = ob.cleaned_data['birthdate']
+            ro = ob.cleaned_data['roll']
+            pw = ob.cleaned_data['password']
+            reg = User(name=nm, email=em, birthdate=bd, roll=ro, password=pw)
+            reg.save()
+            ob = StudentRegistration()
+    else: 
+        ob = StudentRegistration()
+    river = User.objects.all()
+    return render (request, 'enroll/showdata.html', {'form': ob, 'stu': river})
 
 def delete_data(request, id):
     if request.method == 'POST':
