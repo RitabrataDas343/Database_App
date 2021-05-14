8e05ad57c9187e603120a7fe80b3b45494719aed
Ritabrata Das <dasritabrata5@gmail.com>
2021-05-14 19:20:41 +0530

    Update forms.py

--- enroll/forms.py
+++ enroll/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from .models import User
-from django.core import validators
 
 
 class StudentRegistration(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['name', 'email', 'birthdate', 'roll', 'password'] 
+        fields = ['name', 'email', 'birthdate', 'roll', 'password','state', 'district'] 
         widgets= {
             'name': forms.TextInput(attrs={'class':'form-control'}),
             'email': forms.EmailInput(attrs={'class':'form-control'}),
             'birthdate': forms.widgets.DateTimeInput(attrs={"type": "date", 'class':'form-control'}),
             'roll': forms.TextInput(attrs={'class':'form-control'}),
             'password': forms.PasswordInput(attrs={'class':'form-control'}),
+            'state': forms.widgets.Select(attrs={'class':'form-control'}),
+            'district': forms.widgets.Select(attrs={'class':'form-control'}),
         }
 
