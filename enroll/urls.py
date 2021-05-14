0f32c8be9a5db2ec606e835044dd690621e97849
Ritabrata Das <dasritabrata5@gmail.com>
2021-05-14 19:21:22 +0530

    Update urls.py

--- enroll/urls.py
+++ enroll/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
     path('add/', views.add_show, name="addandshow"),
     path('delete/<int:id>/', views.delete_data, name="deletedata"),
     path('<int:id>/', views.update_data, name="updatedata"),
